888267d59f94b628a604f05d842c23da012febb1
ROCKTAKEY <rocktakey@gmail.com>
2022-07-22 09:28:00 +0900

    Initial commit

---
+++ liel.el
@@ -0,0 +1,38 @@
+;;; liel.el --- Lisp interpreter written in Emacs Lisp  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  ROCKTAKEY
+
+;; Author: ROCKTAKEY <rocktakey@gmail.com>
+;; Keywords: lisp
+
+;; Version: 0.0.0
+;; Package-Requires: ((emacs "24.1"))
+;; URL: https://github.com/ROCKTAKEY/liel
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Lisp interpreter written in Emacs Lisp
+
+;;; Code:
+
+(defgroup liel ()
+  "Lisp interpreter written in Emacs Lisp"
+  :group 'lisp
+  :prefix "liel-"
+  :link '(url-link "https://github.com/ROCKTAKEY/liel"))
+
+(provide 'liel)
+;;; liel.el ends here
