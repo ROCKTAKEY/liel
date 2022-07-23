5f39db8dceddec923cb49d3344301e7909f48274
ROCKTAKEY <rocktakey@gmail.com>
2022-07-23 23:02:58 +0900

    Separate reading string token

--- liel.el
+++ liel.el
@@ -58,10 +58,52 @@
   ""
   (plist-get plist :string-escape-char-list))
 
+(defun liel-read-token-string (plist input position)
+  ""
+  (let (char-list
+        close-quote
+        escape?
+        (count position)
+        break
+
+        (token-separator-char-list (liel-get-token-separator-char-list plist))
+        (string-escape-char-list (liel-get-string-escape-char-list plist))
+        (string-quote-char-alist (liel-get-string-quote-char-alist plist)))
+
+    (while (memq (aref input count) token-separator-char-list)
+      (cl-incf count))
+
+    (setq close-quote (alist-get (aref input count) string-quote-char-alist))
+
+    (cl-assert close-quote)
+
+    (cl-incf count)
+
+    (while (not break)
+      (let ((char (aref input count)))
+        (cond
+         (escape?
+          (push char char-list)
+          (setq escape? nil))
+         ((eq char close-quote)
+          (setq break 'in-token))
+         ((memq char string-escape-char-list)
+          (setq escape? t)
+          (push char char-list))
+         (t
+          (push char char-list)))
+
+        (cl-incf count)))
+
+    (list :token (apply #'string (reverse char-list))
+          :input (if (eq break 'in-token)
+                     count
+                   (1- count))
+          :string? t)))
+
 (defun liel-read-token (plist input position)
   ""
   (let (char-list
-        string?
         escape?
         (count position)
         break
@@ -70,66 +112,49 @@
         (new-token-char-list (liel-get-new-token-char-list plist))
         (end-token-char-list (liel-get-end-token-char-list plist))
         (token-separator-char-list (liel-get-token-separator-char-list plist))
-        (string-escape-char-list (liel-get-string-escape-char-list plist))
         (string-quote-char-alist (liel-get-string-quote-char-alist plist)))
 
     (while (memq (aref input count) token-separator-char-list)
       (cl-incf count))
 
-    (while (not break)
-      (let ((char (aref input count)))
-        (cond
-         (string?
+    (if (alist-get (aref input count) string-quote-char-alist)
+        (liel-read-token-string plist input count)
+      (while (not break)
+        (let ((char (aref input count)))
           (cond
            (escape?
             (push char char-list)
             (setq escape? nil))
-           ((eq char string?)
+
+           ((memq char escape-char-list)
+            (push char char-list)
+            (setq escape? t))
+
+           ((memq char token-separator-char-list)
+            (setq break 'out-of-token))
+
+           ((and (null char-list)
+                 (memq char new-token-char-list)
+                 (memq char end-token-char-list))
+            (push char char-list)
             (setq break 'in-token))
-           ((memq char string-escape-char-list)
-            (setq escape? t)
-            (push char char-list))
+
+           ((and char-list
+                 (memq char new-token-char-list))
+            (setq break 'out-of-token))
+
+           ((memq char end-token-char-list)
+            (push char char-list)
+            (setq break 'in-token))
+
            (t
-            (push char char-list))))
-
-         ((and (null char-list)
-               (alist-get char string-quote-char-alist))
-          (setq string? (alist-get char string-quote-char-alist)))
-
-         (escape?
-          (push char char-list)
-          (setq escape? nil))
-
-         ((memq char escape-char-list)
-          (push char char-list)
-          (setq escape? t))
-
-         ((memq char token-separator-char-list)
-          (setq break 'out-of-token))
-
-         ((and (null char-list)
-               (memq char new-token-char-list)
-               (memq char end-token-char-list))
-          (push char char-list)
-          (setq break 'in-token))
-
-         ((and char-list
-               (memq char new-token-char-list))
-          (setq break 'out-of-token))
-
-         ((memq char end-token-char-list)
-          (push char char-list)
-          (setq break 'in-token))
-
-         (t
-          (push char char-list)))
-        (cl-incf count)))
-
-    (list :token (apply #'string (reverse char-list))
-          :input (if (eq break 'in-token)
-                     count
-                   (1- count))
-          :string? string?)))
+            (push char char-list)))
+          (cl-incf count)))
+      (list :token (apply #'string (reverse char-list))
+            :input (if (eq break 'in-token)
+                       count
+                     (1- count))
+            :string? nil))))
 
 (provide 'liel)
 ;;; liel.el ends here
